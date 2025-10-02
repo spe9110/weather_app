af2f326cc45fc1ae0eb7e63279bf92655872ac5c
Spencer WAWAKU <spencerm365@outlook.fr>
2025-10-02 19:33:55 +0200

    Updates

--- Utils/dateHelper.js
+++
@@ -1,4 +0,0 @@
-export function getEnglishWeekday(dateString) {
-  const date = new Date(dateString);
-  return date.toLocaleDateString("en-US", { weekday: "long" });
-}
--- src/components/featuresMenu/CircleSpinner.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./CircleSpinner.css"; // import the CSS
-
-function CircleSpinner() {
-  return <div className="sp-circle"></div>;
-}
-
-export default CircleSpinner;
